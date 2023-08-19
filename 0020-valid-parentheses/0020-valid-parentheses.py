ed167221f77282c79390e1c415c61669a65fdcb7
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-08-18 22:57:10 -0700

    Time: 38 ms (81.38%), Space: 16.4 MB (12.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+             return False
+        stack = []
+        closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return False if len(stack) else True
+            
+
+        
