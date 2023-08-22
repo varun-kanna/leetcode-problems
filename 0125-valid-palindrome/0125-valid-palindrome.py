bdf90e06f909f9e7cd7e639a3922eb5b3cd0bd89
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-08-21 23:27:28 -0700

    Time: 47 ms (87.27%), Space: 17.1 MB (62.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,14 +5,3 @@ class Solution:
             if c.isalnum():
                 newStr += c.lower()
         return newStr == newStr[::-1]
-        # left = 0
-        # right = len(s)-1
-        # i = 0
-        # while i < int(len(s) / 2):
-        #     if s[left] == s[right]:
-        #         left += 1
-        #         right -= 1
-        #         i += 1
-        #     else:
-        #         return False
-        # return True
