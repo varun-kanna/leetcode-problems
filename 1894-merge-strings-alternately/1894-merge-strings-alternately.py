236f5928f277aa34419f0a9d941d138c59260f3d
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-09-12 20:53:47 -0700

    Time: 21 ms (17.19%), Space: 13.4 MB (45.02%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,24 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        merged = ""
-        if len(word1) > len(word2):
-            max_len = len(word1)
-        else:
-            max_len = len(word2)
-        print(max_len)
-        iterative_over = min(len(word1), len(word2))
-        if iterative_over == 0:
-            for i in range(len(word1)):
-                merged += word1[i]
-                merged += word2[i]    
-        else:
-            for i in range(iterative_over):
-                merged += word1[i]
-                merged += word2[i]
+        i,j = 0,0
+        merged = []
 
+        while i < len(word1) and j < len(word2):
+            merged.append(word1[i])
+            merged.append(word2[j])
+            i += 1
+            j += 1
         if len(word1) > len(word2):
-            merged += word1[iterative_over:]
+            merged.append(word1[i:])
         elif len(word2) > len(word1):
-            merged += word2[iterative_over:]
-        return merged
+            merged.append(word2[j:])
+        return "".join(merged)
