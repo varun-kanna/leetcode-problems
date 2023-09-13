e3794b7d872a2e1b13cd4557c6a4501bf69b563c
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-09-12 18:19:28 -0700

    Time: 18 ms (90.79%), Space: 14.1 MB (19.45%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        nums = []
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                nums.append("FizzBuzz")
+            elif i % 3 == 0:
+                nums.append("Fizz")
+            elif i % 5 == 0:
+                nums.append("Buzz")
+            else:
+                nums.append(str(i))
+        return nums
+        
