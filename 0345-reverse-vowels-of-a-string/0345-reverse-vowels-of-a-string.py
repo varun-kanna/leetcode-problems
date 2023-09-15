aaa4f3b71ee86d52f59df5b526d64b309376f620
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-09-14 21:46:39 -0700

    Time: 88 ms (36.91%), Space: 19.9 MB (5.12%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,35 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O" , "U"]
-        # list_words = [str(word) for word in s]
-        # print(list_words)
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O" , "U"]
+        list_words = [str(word) for word in s]
+        print(list_words)
 
-        # left, right = 0, len(list_words) - 1
+        left, right = 0, len(list_words) - 1
 
-        # while left <= right:
-        #     if list_words[right] not in vowels:
-        #         right -= 1
-        #     if list_words[left] not in vowels:
-        #         left += 1
-        #     elif list_words[left] in vowels and list_words[right] in vowels:
-        #         list_words[left], list_words[right] = list_words[right], list_words[left]
-        #         left += 1
-        #         right -= 1
+        while left <= right:
+            if list_words[right] not in vowels:
+                right -= 1
+            if list_words[left] not in vowels:
+                left += 1
+            elif list_words[left] in vowels and list_words[right] in vowels:
+                list_words[left], list_words[right] = list_words[right], list_words[left]
+                left += 1
+                right -= 1
         
-        # return "".join(list_words)
-        s = list(s)
-        vowels = 'aeiouAEIOU'
-        l, r = 0, len(s) - 1
-
-        while (l < r):
-            while (l < r and s[l] not in vowels):
-                l += 1
-            while (r > l and s[r] not in vowels):
-                r -= 1
-            s [l], s[r] = s[r], s[l]
-
-            l +=1
-            r -=1
-            
-        return "".join(s)
+        return "".join(list_words)
+        
