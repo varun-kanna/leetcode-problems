30158f9b0ad2e97eda1ee11772c5891f1758685c
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-09-14 21:09:13 -0700

    Time: 114 ms (82.8%), Space: 14 MB (49.52%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -5,9 +5,17 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        f = [0] + flowerbed + [0]
-        for i in range(1, len(f) - 1):
-            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
-                f[i] = 1
+        # This requires extra memory since we make a new array, O(n) space complexity
+        # f = [0] + flowerbed + [0]
+        # for i in range(1, len(f) - 1):
+        #     if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+        #         f[i] = 1
+        #         n -= 1
+        # return n <= 0
+
+        # This requires no additonal space, so constant O(1) space complexity
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed) -1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
                 n -= 1
         return n <= 0
